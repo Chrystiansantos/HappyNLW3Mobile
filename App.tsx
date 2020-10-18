a768d7ae498ef295fc51f04864aab5357b786879
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-10-18 14:31:19 -0300

    FInalizando quarto dia NLW

--- App.tsx
+++ App.tsx
@@ -1,37 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
-import mapMarker from './src/assets/images/MapMarker.png';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -20.1784106,
-          longitude: -45.7094105,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          coordinate={{ latitude: -20.1784106, longitude: -45.7094105 }}
-        />
-      </MapView>
-    </View>
-  );
-}
+import Routes from './src/routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+const App: React.FC = () => {
+  const [fontsLoade] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoade) {
+    return null;
+  }
+  return <Routes />;
+};
+
+export default App;
